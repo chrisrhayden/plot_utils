641b13b27e63d6b87239e48ae6faf4b5756d8a27
chris <chrisrhayden@gmail.com>
2019-06-14 14:34:56 -0700

    fix setup

--- setup.py
+++ setup.py
@@ -4,5 +4,6 @@ from distutils.core import setup
 
 setup(name='plot_utils',
       py_modules=['rectangle'],
+      packages=['plot_utils'],
       install_requires=['matplotlib', 'numpy'],
       )
